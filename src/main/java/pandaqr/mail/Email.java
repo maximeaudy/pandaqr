3a46bf7ee35713dfc8405dc2d6d836274ad8225c
Emixham <Maximed.ipod@gmail.com>
2020-01-15 09:55:00 +0100

    Finish

--- src/main/java/pandaqr/mail/Email.java
+++ src/main/java/pandaqr/mail/Email.java
@@ -1,4 +1,5 @@
 package pandaqr.mail;
+import java.util.Date;
 import java.util.Properties;
 
 import javax.activation.DataHandler;
@@ -9,28 +10,40 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
 
-public class Email {
+import net.bytebuddy.agent.builder.AgentBuilder.InitializationStrategy.SelfInjection.Split;
 
+//import pandaqr.calendar.gmail;
+
+public class Email {
+	private Date dateDebut;
+	private Date dateFin;
+	private String mailOrganisateur;
+	private int numSalle;
+	private String description;
+	private String titre;
+	String listParticipants;
     public Email() {
+
     }
 
     /*
      * @param args
      */
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         try {
             Email email = new Email();
-            email.send();
+            email.send("maximed.ipod@gmail.com","nouvelle sieste", "je sens que cette sieste va être une pure soirée","20191213T163000Z","20191213T170000Zo","maximedelx@gmail.com",105);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
-    public void send() throws Exception {
+    public void send(String listParticipants, String titre, String description, String dateDebut,String dateFin,String mailOrganisateur,int numSalle) throws Exception {
 
         try {
             String from = "pandaqr1@gmail.com";
-            String to = "leahelary03@gmail.com";
+            String to = "pandaqr1@gmail.com";
+            String bcc = listParticipants;
             final String username = "pandaqr1@gmail.com";//change accordingly
             final String password = "Azerty11+";//change accordingly
             
@@ -55,8 +68,23 @@ public class Email {
 
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject("Invitation à la réunion du 12/12/2019");
-
+            if (bcc.contains(";")) {
+            	String[] dest ;
+            	dest = bcc.split(";");
+            	for(String destinataire : dest){
+            		message.addRecipient(Message.RecipientType.BCC, new InternetAddress(destinataire));
+            	}
+            }else {
+            	message.addRecipient(Message.RecipientType.BCC, new InternetAddress(bcc));
+            }
+            
+            message.setSubject("Invitation à la réunion du "+dateDebut);
+            message.setText("Bonjour,\n"
+            		+ "vous êtes invité à une nouvelle réunion."
+            		+ "Celle ci commence le "+dateDebut+". "
+                    + "Cordialement,"
+                    + "PandaQR","utf-8","html");
+            
             StringBuffer sb = new StringBuffer();
 
             StringBuffer buffer = sb.append("BEGIN:VCALENDAR\n" +
@@ -64,11 +92,11 @@ public class Email {
                     "VERSION:2.0\n" +
                     "METHOD:REQUEST\n" +
                     "BEGIN:VEVENT\n" +
-                    "ATTENDEE;ROLE=REQ-PARTICIPANT;RSVP=TRUE:MAILTO:leahelary03@gmail.com\n" +
-                    "ORGANIZER:MAILTO:xx@xx.com\n" +
-                    "DTSTART:20191212T153000Z\n" +
-                    "DTEND:20191212T163000Z\n" +
-                    "LOCATION:SALLE 105\n" +
+                    "ATTENDEE;ROLE=REQ-PARTICIPANT;RSVP=TRUE:MAILTO:"+listParticipants+"\n" +
+                    "ORGANIZER:MAILTO:"+mailOrganisateur+"\n" +
+                    "DTSTART:"+dateDebut+"\n" +
+                    "DTEND:"+dateFin+"\n" +
+                    "LOCATION:SALLE "+numSalle+"\n" +
                     "TRANSP:OPAQUE\n" +
                     "SEQUENCE:0\n" +
                     "UID:040000008200E00074C5B7101A82E00800000000002FF466CE3AC5010000000000000000100\n" +
@@ -76,10 +104,10 @@ public class Email {
                     "DTSTAMP:20051206T120102Z\n" +
                     "CATEGORIES:Réunion\n" +
                     //description
-                    "DESCRIPTION:Petite sieste entre collaborateurs\n\n" +
+                    "DESCRIPTION:"+description+"\n" +
                     // titre
-                    "SUMMARY:oui\n" +
-                    "PRIORITY:9\n" +
+                    "SUMMARY:"+titre+"\n" +
+                    "PRIORITY:5\n" +
                     "CLASS:PUBLIC\n" +
                     "BEGIN:VALARM\n" +
                     "TRIGGER:PT1440M\n" +
@@ -97,9 +125,18 @@ public class Email {
             messageBodyPart.setDataHandler(new DataHandler(
                     new ByteArrayDataSource(buffer.toString(), "text/calendar")));// very important
 
+            BodyPart msgBodyPart = new MimeBodyPart();
+
+            // Fill the message
+            msgBodyPart.setText("Bonjour,"+ "vous êtes invité à une nouvelle réunion.\n"+ "Celle ci commence le "+dateDebut+". \n"+ "Cordialement,\n"+ "PandaQR");
+
             // Create a Multipart
             Multipart multipart = new MimeMultipart();
+            
+            // Add part one
+            multipart.addBodyPart(msgBodyPart);
 
+            
             // Add part one
             multipart.addBodyPart(messageBodyPart);
 
